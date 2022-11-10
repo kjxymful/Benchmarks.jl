0aa35329ed6b02bc7da1735b9b932241ad3aa273
Patrick Leibersperger <pleibersperger@posteo.de>
2022-11-10 14:28:22 +0100

    test integrators, and add 2nd defaults

--- test/testing_grounds/bursting_test.jl
+++ test/testing_grounds/bursting_test.jl
@@ -25,44 +25,45 @@ kₘ=5
 τₕ=200
 gₙₘ₀ₐ=10.1
 p = [I, Cₘ, gₗ, Eₗ, gₙₐ, Eₙₐ, Vₕₙₐ, kₙₐ, gₖ, Eₖ, Vₕₖ, kₖ, τₙ, gₘ, Vₕₘ, kₘ, τₕ, gₙₘ₀ₐ]
-tspan = (0.0, 10000.0)
+tspan = (0.0, 1000.0)
 
+# [-1.33192587  0.00833237  0.01267124]
 # differential DifferentialEquations
-prob = ODEProblem(loop_burstn!, [-60.0, 0.0386, 0.0231], tspan, p)
+prob = ODEProblem(loop_burstn!,[-20,0.5,0.03], tspan, p)
 sol = solve(prob)
-time = 1000:0.01:10000
+time = 100:0.01:1000
 u = [sol(t) for t in time]
 u = permutedims(hcat(u...))
 p1 = plot3d(u[:, 1], u[:, 2], u[:, 3], label="DifferentialEquations")
 
 # DynamicalSystems
 bn = ContinuousDynamicalSystem(loop_burstn!, [-20,0.5,0.03], p)
-u3 = trajectory(bn,10000, Δt=0.01, Ttr=1000)
+u3 = trajectory(bn,1000, Δt=0.01, Ttr=100)
 plot3d!(u3[:,1],u3[:,2],u3[:,3],label="DynamicalSystems")
 
-#display(p1)
+display(p1)
 # savefig(p1,"test/comparison_integrators.png")
 
-# ns ns_bursting_neuron
-pns = [x->linear(2,4, 1500,x-500)]
-ns_loop = ContinuousDynamicalSystem(ns_bursting_neuron!, [-20,0.5,0.03], pns)
-u2 = generate_trajectories(ns_loop, 1000,500,PLOT=false)
-# u2 = trajectory(ns_loop, 1000, Δt = 0.01, Ttr=500)
-p2=plot3d(u2[:,1],u2[:,2],u2[:,3], lc=(:viridis),linez=collect(0:0.01:1000))
-# display(p2)
+# # ns ns_bursting_neuron
+# pns = [x->linear(2,4, 1500,x-500)]
+# ns_loop = ContinuousDynamicalSystem(ns_bursting_neuron!, [-20,0.5,0.03], pns)
+# u2 = generate_trajectories(ns_loop, 1000,500,PLOT=false)
+# # u2 = trajectory(ns_loop, 1000, Δt = 0.01, Ttr=500)
+# p2=plot3d(u2[:,1],u2[:,2],u2[:,3], lc=(:viridis),linez=collect(0:0.01:1000))
+# # display(p2)
 
-# snapshot
-pt = copy(p)
-pt[end] = 2
-bnt = ContinuousDynamicalSystem(loop_burstn!, [-20,0.5,0.03], pt)
-ut = trajectory(bnt, 1500, Δt=0.01,Ttr=5)
-ut2 = generate_trajectories(bnt, 1500, 5,PLOT=false)
+# # snapshot
+# pt = copy(p)
+# pt[end] = 2
+# bnt = ContinuousDynamicalSystem(loop_burstn!, [-20,0.5,0.03], pt)
+# ut = trajectory(bnt, 1500, Δt=0.01,Ttr=5)
+# ut2 = generate_trajectories(bnt, 1500, 5,PLOT=false)
 
-plot1 = plot3d(ut[:,1],ut[:,2],ut[:,3])
-plot2 = plot3d(ut2[:,1],ut2[:,2],ut2[:,3])
-plot3 = plot3d!(plot1, ut2[:,1],ut2[:,2],ut2[:,3],label="gen")
-end_plot = Plots.plot(plot1, plot2, plot3, layout=3, title=["test" "gen fun" "both"])
-# display(end_plot)
+# plot1 = plot3d(ut[:,1],ut[:,2],ut[:,3])
+# plot2 = plot3d(ut2[:,1],ut2[:,2],ut2[:,3])
+# plot3 = plot3d!(plot1, ut2[:,1],ut2[:,2],ut2[:,3],label="gen")
+# end_plot = Plots.plot(plot1, plot2, plot3, layout=3, title=["test" "gen fun" "both"])
+# # display(end_plot)
 
 
 # shrinking lorenz
